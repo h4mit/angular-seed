1766f0ad7c459ed9b3fdcd42e7167b66654ad9b8
H4mit <h4mid.taheri@gmail.com>
2017-08-27 19:01:33 +0430

    fixed view routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import {PublicLayout} from "./public/layout/public.layout";
 import {PanelLayout} from "./panel/layout/panel.layout";
 import {PanelDashboard} from "./panel/dashboard/dashboard";
 import {NotFoundPage} from "./public/not-found-page/not-found-page";
+import {AppComponent} from "./app";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     PublicLayout,
     PanelLayout,
     PanelDashboard,
-    NotFoundPage
+    NotFoundPage,
+    AppComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     ShareModule,
     routing,
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+/**
+ * Created by h4mit on 27/08/2017.
+ */
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: './app.html'
+})
+export class AppComponent {
+  constructor() {
+  }
+}
